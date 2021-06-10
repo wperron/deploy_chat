9470f36c2f8de9275fde709fff8d3d11a29e26e2
William Perron <hey@wperron.io>
2021-06-10 10:49:56 -0400

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export * from "https://deno.land/x/sift@0.3.1/mod.ts";
+export * from "https://deno.land/x/sift@0.3.2/mod.ts";
 export { generate as generateUUID } from "https://deno.land/std@0.97.0/uuid/v4.ts";
 export {
   getCookies,
